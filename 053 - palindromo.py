4941eed4dde0acad545a995086e0cb0d1f4ef4ab
luiz carlos <luizcarslossf564@gmail.com>
2024-01-11 10:38:08 -0300

    053 mundo2.py

---
+++ 053 - palindromo.py
@@ -0,0 +1,13 @@
+frase =str(input('digite uma frase: ')).strip().upper()
+palavra = frase.split()
+junto = "".join(palavra)
+inverso = ""
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print(f'O inverso de {junto} é {inverso}')
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palindromo')
+
+
